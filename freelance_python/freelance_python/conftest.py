05b63dd31fdee9b30558437b53492428929f40a8
Musin Dmitriy <me@pythonworld.ru>
2021-01-23 20:22:00 +0300

    Initial commit

---
+++ freelance_python/freelance_python/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from freelance_python.users.models import User
+from freelance_python.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
